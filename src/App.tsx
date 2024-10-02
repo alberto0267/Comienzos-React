5ff04b3092e5c657762c8bb23424245300a7518e
BlancoAlberto <blancoalberto.r@gmail.com>
2024-10-02 12:33:31 +0200

    React

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,52 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+const user = {
+
+  name : 'Geralt Rivia',
+  imageUrl: 'https://www.ecured.cu/images/f/ff/Geralt_de_Rivia.jpg',
+  imageSize : 90,
+};
+
+function MyButton() {
+  return (
+    <button className='ButtomTest'>Soy un botón</button>
+  );
+}
+
+function LoginForm(){
+  return(
+    <p>Login Form</p>
+  )
+}
+
+function LoginAdmin(){
+  return(
+    <p>Login Admin</p>
+  )
+}
+
+
 function App() {
   const [count, setCount] = useState(0)
+  const [IsForm, setIsForm] = useState(false);
 
+let content;
+if(IsForm){
+  content = <LoginForm />
+}else{
+  content = <LoginAdmin />
+}
+
+
+
+
+// const showTitle = count>5?<p>Hola mundo</p>:<h1>jajajaj + React</h1>
   return (
     <>
       <div>
+        <h1>Prueba</h1>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,8 +56,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>jajajaj + React</h1>
-      <div className="card">
+        {/* {count>5?<p>Hola mundo</p>:<h1>jajajaj + React</h1>} */}
+      <div className={count>=2?"card":"  hola"}>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -28,8 +68,30 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+       {/* Aquí estamos llamando al componente App2 */}
+       <h2>Bienvenido a mi Aplicacion desde aqui</h2>
+       <MyButton />
+
+      <h2>{user.name}</h2>
+      <img src={user.imageUrl} alt="" className='ImageGeralt'/>
+
+    {/* Llamo a renderizado */}
+
+
+<div><button onClick={()=> setIsForm(!IsForm)}>
+ {IsForm ? 'Brujo' : 'Carnicero'}
+ </button>
+ </div>
+ 
+
     </>
   )
+
+
+
 }
+
+
 
 export default App
